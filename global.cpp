4b84fd826444857d98669b154901ba54f30121c5
Marcin Kolodziej <gotennn@gmail.com>
2014-04-05 17:54:38 +0200

    renaming ingredient -> paint

--- global.cpp
+++ global.cpp
@@ -1,11 +1,11 @@
 #include "global.h"
 
-std::vector<pthread_mutex_t> ingredients_mutex;
+std::vector<pthread_mutex_t> paint_mutex;
 std::vector<pthread_mutex_t> brush_mutex;
-std::vector<pthread_cond_t>  ingredients_cond;
+std::vector<pthread_cond_t>  paint_cond;
 std::vector<pthread_cond_t>  brush_cond;
 
-std::vector<int> ingredients_count;
+std::vector<int> paint_count;
 std::vector<int> brush_usage_side;
 
 int brush_count;
--- painter.h
+++ painter.h
@@ -14,19 +14,19 @@ class Painter {
     // -1 - right, 1 - left, 0 - clear
     int _brush_side;
 
-    int _brush_ind, _ingredient_ind;
+    int _brush_ind, _paint_ind;
 
 public:
     int number;
     Painter() {};
-    Painter(int i, int ingredient_ind, int brush_ind);
+    Painter(int i, int paint_ind, int brush_ind);
 
     void work();
 
 private:
     void produceGold();
 
-    void lockIngredient();
+    void lockPaint();
     void lockBrush();
     void releaseStuff();
 };
